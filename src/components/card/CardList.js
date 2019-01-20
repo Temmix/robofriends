0a89bf3d752b44e296b399fd73fc4818be315115
Temi Makinde <Temi.Makinde@Gmail.com>
2019-01-20 20:46:52 +0000

    app init

---
+++ src/components/card/CardList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => { 
+    return (
+        <div>
+          {
+              robots.map((x,i) => 
+                <Card 
+                    key={i} 
+                    id={x.id} 
+                    email={x.email} 
+                    name={x.name}/>
+                )
+          }
+         </div>
+        );
+}
+
+export default CardList;

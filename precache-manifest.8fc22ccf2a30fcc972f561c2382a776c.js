97a6d34b7ecc5e8df27dad3822ca8f83d48caa3b
Temi Makinde <Temi.Makinde@Gmail.com>
2019-01-27 19:00:14 +0000

    Updates

---
+++ precache-manifest.8fc22ccf2a30fcc972f561c2382a776c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "a1d18a3c15c40ff449ee",
+    "url": "/robofriends/static/js/main.a1d18a3c.chunk.js"
+  },
+  {
+    "revision": "a93f2544e10698a3d99a",
+    "url": "/robofriends/static/js/1.a93f2544.chunk.js"
+  },
+  {
+    "revision": "a1d18a3c15c40ff449ee",
+    "url": "/robofriends/static/css/main.8adaf2fe.chunk.css"
+  },
+  {
+    "revision": "a93f2544e10698a3d99a",
+    "url": "/robofriends/static/css/1.250b2f14.chunk.css"
+  },
+  {
+    "revision": "0ea7df081a412b4bb3a88ef1b8c4c80a",
+    "url": "/robofriends/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c458e629790060e01f85642e4cd21d7b.js"
+  "/robofriends/precache-manifest.8fc22ccf2a30fcc972f561c2382a776c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/robofriends/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

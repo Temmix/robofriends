0ae656e0fdb15b5cf2faa558b1af43029f2bd755
Temi Makinde <Temi.Makinde@Gmail.com>
2019-03-13 17:21:39 +0000

    changes to mainpage.js

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,39 +1,40 @@
-import React, {Component} from 'react';  
+import React, { Component } from "react";
 
-import CardList from '../card/CardList';
-import SearchBox from '../searchBox/SearchBox';
-import Scroll from '../scroll/Scroll';
-import ErrorBoundry from '../errorBoundry/ErrorBoundry'; 
-import './Mainpage.css'
+import CardList from "../card/CardList";
+import SearchBox from "../searchBox/SearchBox";
+import Scroll from "../scroll/Scroll";
+import ErrorBoundry from "../errorBoundry/ErrorBoundry";
+import "./Mainpage.css";
 
+class MainPage extends Component {
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
 
-class MainPage extends Component { 
-    componentDidMount(){
-        this.props.onRequestRobots();
-    }
- 
-    filterRobots = () => {
-        return this.props.robots.filter(x => x.name.toLowerCase().includes(this.props.searchField.toLowerCase()))
-    }
+  filterRobots = () => {
+    return this.props.robots.filter(x =>
+      x.name.toLowerCase().includes(this.props.searchField.toLowerCase())
+    );
+  };
 
-    render() {
-        const { robots, onSearchChange, isPending } = this.props; 
-            return (
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChanged={onSearchChange} />
-                    <Scroll>
-                        { isPending 
-                            ?  <h1>Loading ...</h1> 
-                            :  <ErrorBoundry>
-                                    <CardList robots={this.filterRobots(robots)}/>
-                                </ErrorBoundry>
-                        }
-                    </Scroll>
-                </div> 
-            )
-        
-    }
+  render() {
+    const { robots, onSearchChange, isPending } = this.props;
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChanged={onSearchChange} />
+        <Scroll>
+          {isPending ? (
+            <h1>Loading ...</h1>
+          ) : (
+            <ErrorBoundry>
+              <CardList robots={this.filterRobots(robots)} />
+            </ErrorBoundry>
+          )}
+        </Scroll>
+      </div>
+    );
+  }
 }
 
 export default MainPage;
